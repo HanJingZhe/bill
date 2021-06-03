53fa5f21e4db41a7e0481e44f67cebed06e5a958
peng_wang <qtgm386800265@163.com>
2021-06-03 23:11:16 +0800

    initialized

---
+++ app/src/main/java/com/qtgm/bill/BillApplication.kt
@@ -0,0 +1,13 @@
+package com.qtgm.bill
+
+import android.app.Application
+
+class BillApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+
+}
---
+++ app/src/main/java/com/qtgm/bill/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.qtgm.bill
+
+import com.qtgm.base.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : BaseActivity() {
+
+    override fun setLayoutId(): Int = R.layout.activity_main
+
+
+    override fun initView() {
+        tvShow.text = "hello bill!"
+    }
+
+    override fun initData() {
+    }
+
+
+}
